9b2a0b01e581fc1b6324a9eddaca6f3f606905bd
Raj kiran <120042791+djraj47@users.noreply.github.com>
2023-09-18 00:02:41 +0530

    18 sep

---
+++ Notes-App/script.js
@@ -0,0 +1,45 @@
+const notesContainer = document.querySelector(".notes-container");
+const createBtn = document.querySelector(".btn");
+let notes = document.querySelectorAll(".input-box");
+
+function showNotes() {
+    notesContainer.innerHTML = localStorage.getItem("notes");
+}
+showNotes();
+function updateStorage() {
+    localStorage.setItem("notes", notesContainer.innerHTML);
+}
+
+createBtn.addEventListener("click", () => {
+    let inputBox = document.createElement("p");
+    let img = document.createElement("img");
+    inputBox.className = "input-box";
+    inputBox.setAttribute("contenteditable", "true")
+    img.src = "images/delete.png";
+    notesContainer.appendChild(inputBox).appendChild(img);
+
+
+})
+
+// for delete function
+notesContainer.addEventListener("click", (e) => {
+    if (e.target.tagName === "IMG") {
+        e.target.parentElement.remove();
+        updateStorage();
+    }
+    else if (e.target.tagName === "P") {
+        notes = document.querySelectorAll(".input-box")
+        notes.forEach(nt=> {
+            nt.onkeyup = function () {
+                updateStorage();
+            }
+        })
+    }
+})
+
+document.addEventListener("keyDown" , (event)=>{
+    if(event.key === "Enter"){ 
+        document.execCommand("insertLinerBreak");
+        event.preventDefault();
+    }
+})
